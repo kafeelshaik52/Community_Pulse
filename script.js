08dfd06264148995596061e35f44cf2acf678992
Kafeel_Shaik <kafeelshaik52@gmail.com>
2026-06-09 10:02:59 +0530

    commit_3

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 HTML = Structure
 CSS = Design
 JS = Nothing
+
+
+const questions = document.querySelectorAll(".faq-question");
+
+questions.forEach(question => {
+
+    question.addEventListener("click", ()=>{
+
+        question.parentElement.classList.toggle("active");
+
+    });
+
+});
